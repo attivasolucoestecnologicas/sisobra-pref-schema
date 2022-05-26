db5adf9ff2c8e0aa363e4ece4306000808150e57
Walber Jefferson <walberjefferson@hotmail.com>
2022-05-26 10:56:51 -0300

    Finalizando Lote

--- src/LoteAlvaraHabitese.php
+++ src/LoteAlvaraHabitese.php
@@ -15,26 +15,26 @@ class LoteAlvaraHabitese extends Lote implements Xml
         $versao = $this->xml->createElement('versao', '1.0.1');
         $pai->appendChild($versao);
 
-        $alvara = $this->xml->createElement('Alvara');
-        foreach ($this->alvara as $a) {
-            $alvaraNode = new \Attiva\SisObraXML\Factory\Alvara($a);
-            $xml = $this->xml->importNode($alvaraNode->xml(), true);
-            $alvara->appendChild($xml);
-        }
-
-        $habitese = $this->xml->createElement('Habitese');
-        foreach ($this->habitese as $h) {
-            $habiteseNode = new \Attiva\SisObraXML\Factory\Habitese($h);
-            $xml = $this->xml->importNode($habiteseNode->xml(), true);
-            $habitese->appendChild($xml);
-        }
-
-        $alteracao = $this->xml->createElement('AlteraSituacao');
-        foreach ($this->alteracao as $a) {
-            $alteracaoNode = new \Attiva\SisObraXML\Factory\AlteraSituacao($a);
-            $xml = $this->xml->importNode($alteracaoNode->xml(), true);
-            $alteracao->appendChild($xml);
-        }
+//        $alvara = $this->xml->createElement('Alvara');
+//        foreach ($this->alvara as $a) {
+//            $alvaraNode = new \Attiva\SisObraXML\Factory\Alvara($a);
+//            $xml = $this->xml->importNode($alvaraNode->xml(), true);
+//            $alvara->appendChild($xml);
+//        }
+//
+//        $habitese = $this->xml->createElement('Habitese');
+//        foreach ($this->habitese as $h) {
+//            $habiteseNode = new \Attiva\SisObraXML\Factory\Habitese($h);
+//            $xml = $this->xml->importNode($habiteseNode->xml(), true);
+//            $habitese->appendChild($xml);
+//        }
+//
+//        $alteracao = $this->xml->createElement('AlteraSituacao');
+//        foreach ($this->alteracao as $a) {
+//            $alteracaoNode = new \Attiva\SisObraXML\Factory\AlteraSituacao($a);
+//            $xml = $this->xml->importNode($alteracaoNode->xml(), true);
+//            $alteracao->appendChild($xml);
+//        }
 
         $this->xml->saveXML("LoteAlvaraHabitese.xml");
     }
